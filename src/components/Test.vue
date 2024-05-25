1991cee6fe9f0a8fd56c2065d042eb902da43bbb
gabrielle.tsai <linda41436@gmail.com>
2024-05-25 21:51:09 +0800

    Build App and set routes for datas.

---
+++ src/components/Test.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="test">
+        我是測試元件中的內容 {{msg}}
+    </div>
+</template>
+
+<script>
+export default {
+    data () {
+        return {
+            msg: '數據'
+        }
+    }
+}
+</script>
+
+<style>
+    .test {
+        color: rosybrown;
+    }
+</style>
